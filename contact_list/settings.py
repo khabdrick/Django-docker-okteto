c1f090ef97135752f9dd545ee4ab740c81e6e99a
Muhammed Ali <muhamzyali@gmail.com>
2022-06-22 19:44:56 +0100

    improve settings file

--- contact_list/settings.py
+++ contact_list/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=#m*4-6w^mpd8%_=(pjnxh3)z*_ni!0e-wr(^$8ndci58f-oh7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
