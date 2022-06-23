0585d2045637a8737da390e931e2d3712d912bf7
Muhammed Ali <muhamzyali@gmail.com>
2022-06-23 14:46:49 +0100

    views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .serializers import ContactListSerializer
+from rest_framework.response import Response
+from .models import ContactList
 
-# Create your views here.
+@api_view(['POST'])
+def create_contact(request):
+        # create coontact
+    contact = ContactListSerializer(data=request.data)
+    if contact.is_valid():
+        contact.save()
+        return Response(contact.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+class ContactListView(generics.ListCreateAPIView):
+    # list out contacts
+    queryset = ContactList.objects.all()
+    serializer_class = ContactListSerializer
